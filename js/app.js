aef498acca72b953d08900a15adb33a29a99a8ca
MollyGreace <greace.ss@gmail.com>
2017-11-19 23:38:36 -0500

    modificando archivos

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',function(){
+/*Varaibles */
+  var input = document.querySelector('input');
+
+/*CRea un tablero grande*/
+  input.addEventListener('click',function(){
+    input.placeholder='';
+    var changeInput = document.seta('input');
+    console.log(changeInput);
+
+  })
+});  
